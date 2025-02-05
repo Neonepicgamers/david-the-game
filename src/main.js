b24d681766ebe8cf9b9e38d8d7948d90b313dc23
Neonepicgamers <neonepicgamers@gmail.com>
2025-02-05 12:59:11 -0500

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 const tickRate = 1000 / 30;
 let clickStrength = 1;
-let score = 0;
+let score = 999999999999999999;
 
-let homelessperson = new Building('Homeless Person', 0.5, 15, "../media/imgs/homeless.jpg");
-let dingus = new Building('Dingus', 5, 200, "../media/imgs/dingus.jpg");
-let realsurgeon = new Building('Real Surgeon', 15, 500, "../media/imgs/surgeon.png");
-let davidclone = new Building('David Clone', 50, 1000, "../media/imgs/davidcln.jpg");
+let homelessperson = new Building('Homeless Person', 0.1, 15, "../media/imgs/homeless.jpg");
+let dingus = new Building('Dingus', 1, 100, "../media/imgs/dingus.jpg");
+let realsurgeon = new Building('Real Surgeon', 8, 1100, "../media/imgs/surgeon.png");
+let davidclone = new Building('David Clone', 47, 12000, "../media/imgs/davidcln.jpg");
+let sawnick = new Building('Sawnick', 260, 130000, "../media/imgs/sawnick.gif");
+let ourpleguy = new Building('Ourple Guy', 1400, 14000000, "../media/imgs/ourpguy.gif");
 
-let whip1 = new Whip('Whip I', 200, homelessperson);
+let whip1 = new Whip('Whip I', 100, homelessperson);
 
 function scorePlusPlus() {
     score+= clickStrength;
@@ -18,6 +20,8 @@ function incScore() {
     score += dingus.cps;
     score += realsurgeon.cps;
     score += davidclone.cps;
+    score += sawnick.cps;
+    score += ourpleguy.cps;
 }
 
 function updateButtons() {
@@ -25,6 +29,8 @@ function updateButtons() {
     dingus.buttonState();
     realsurgeon.buttonState();
     davidclone.buttonState();
+    sawnick.buttonState();
+    ourpleguy.buttonState();
     whip1.buttonState();
 }
 
